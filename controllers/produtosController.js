141e075750d7fd4397977e6c07268e78c5200850
AlexssandroFS <47816933+AlexssandroFS@users.noreply.github.com>
2024-03-03 21:08:57 -0300

    Update produtosController.js

--- controllers/produtosController.js
+++ controllers/produtosController.js
@@ -1138,7 +1138,7 @@ roteador.get('/estoquetotal', async(req, res) => {
 
             ],
             order: ['id'],
-            group: ['produtosidentradas', 'id'],
+            group: ['produtosidentradas', 'id_saidas'],
             distinct: true,
             raw: true,
         });
@@ -1187,7 +1187,7 @@ roteador.get('/estoquetotal', async(req, res) => {
 
             ],
             order: ['id'],
-            group: ['produtosid', 'id'],
+            group: ['produtosid', 'id_produtos'],
             distinct: true,
             raw: true,
         });
@@ -1270,7 +1270,7 @@ roteador.get('/estoqueminimo', async(req, res) => {
 
             ],
             order: ['id'],
-            groupBy: ['produtosidentradas', 'id'],
+            groupBy: ['produtosidentradas', 'id_saidas'],
             distinct: true,
             raw: true,
         });
@@ -1320,7 +1320,7 @@ roteador.get('/estoqueminimo', async(req, res) => {
             ],
 
             order: ['nomeprod'],
-            group: ['produtosid', 'id'],
+            group: ['produtosid', 'id_produtos'],
             distinct: true,
             raw: true,
         });
